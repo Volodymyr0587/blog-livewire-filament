04e16afba166cd384f5aff0c889af4be70e36920
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-06 17:29:46 +0200

    create livewire component PostList; display posts with pagination

---
+++ app/Livewire/PostList.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class PostList extends Component
+{
+    use WithPagination;
+
+    #[Computed()]
+    public function posts()
+    {
+        return Post::published()->orderBy('published_at', 'desc')->paginate(5);
+    }
+
+    public function render()
+    {
+        return view('livewire.post-list');
+    }
+}
