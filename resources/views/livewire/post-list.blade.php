b1de22096c457baa30879a6d29cc89f6542fe9ff
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-11 12:36:13 +0200

    Add svg to clear search result button

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -2,8 +2,10 @@
     <div class="flex justify-between items-center border-b border-gray-100">
         <div class="text-gray-600">
             @if ($this->activeCategory || $search)
-                <button class="text-gray-500 text-xs mr-3 p-1 border-2 border-red-800"
-                    wire:click="clearFilters()">X</button>
+                <button class=""
+                    wire:click="clearFilters()">
+                    <svg class="h-6" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20 20h-15.25c-.414 0-.75.336-.75.75s.336.75.75.75h15.75c.53 0 1-.47 1-1v-15.75c0-.414-.336-.75-.75-.75s-.75.336-.75.75zm-1-17c0-.478-.379-1-1-1h-15c-.62 0-1 .519-1 1v15c0 .621.52 1 1 1h15c.478 0 1-.379 1-1zm-15.5.5h14v14h-14zm7 5.937 2.219-2.22c.146-.146.339-.219.531-.219.404 0 .75.325.75.75 0 .193-.073.384-.219.531l-2.22 2.22 2.222 2.222c.147.147.22.339.22.53 0 .427-.349.751-.75.751-.192 0-.384-.073-.53-.219l-2.223-2.223-2.223 2.223c-.146.146-.338.219-.53.219-.401 0-.75-.324-.75-.751 0-.191.073-.383.22-.53l2.222-2.222-2.22-2.22c-.146-.147-.219-.338-.219-.531 0-.425.346-.75.75-.75.192 0 .385.073.531.219z" fill-rule="nonzero"/></svg>
+                </button>
             @endif
             @if ($this->activeCategory)
                 <x-badge wire:navigate href="{{ route('posts.index', ['category' => $this->activeCategory->slug]) }}"
