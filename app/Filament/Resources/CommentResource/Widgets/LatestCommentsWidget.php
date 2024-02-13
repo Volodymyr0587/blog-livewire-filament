c0e319d1c67050340ec289d735aa6853a7adc65f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-13 13:28:42 +0200

    View comment

--- app/Filament/Resources/CommentResource/Widgets/LatestCommentsWidget.php
+++ app/Filament/Resources/CommentResource/Widgets/LatestCommentsWidget.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\CommentResource\Widgets;
 
+use App\Filament\Resources\CommentResource;
 use App\Models\Comment;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -23,6 +25,11 @@ class LatestCommentsWidget extends BaseWidget
                 TextColumn::make('post.title'),
                 TextColumn::make('comment'),
                 TextColumn::make('created_at')->date()->sortable(),
+            ])
+            ->actions([
+                Action::make('View')
+                ->url(fn (Comment $record) : string => CommentResource::getUrl('edit', ['record' => $record]))
+                ->openUrlInNewTab()
             ]);
     }
 }
